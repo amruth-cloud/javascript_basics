2ab1f750248d330f9cc913089d642deefa34d90f
Amruth Venkatasubramanya <amruth963@outlook.com>
2026-03-10 20:58:37 +0000

    added var, let, const variable comparison

--- src/JS02variables.js
+++ src/JS02variables.js
@@ -1,9 +1,9 @@
 //var, let and const
 
-/**
- * 1. var: used in old JS and it has limited usage in the modern JS file coz duplicate variables is allowed
- * 2. var is function scoped and can be redeclared and updated
- * 3. var variables are hoisted to the top of their scope and initialized with undefined
+/** var usage
+ * 1. used in old JS and it has limited usage in the modern JS file coz duplicate variables is allowed
+ * 2. is function scoped and can be redeclared and updated
+ * 3. variables are hoisted to the top of their scope and initialized with undefined
  */
 
 //Re-Declaration of same variable name is allowed
@@ -31,6 +31,41 @@ console.log(temp);//undefined
 var temp = 90;
 console.log(temp);
 
-/**
- * 
+/** let usage
+ * cannot be re declared
+ * Reassigment for the same variable is allowed
+ * Hoisiting is not allowed without declaration
  */
+
+//Re-declaration of same variable name is not allowed, it throws an error in while compiling code
+let x = 10;
+//let x = 20; // SyntaxError: Identifier 'x' has already been declared
+
+//Reassignment for the same variable is allowed
+let y = 10;
+y = 20;
+
+//Hoisiting is not allowed without declaration as it throws an error
+//console.log(z); // ReferenceError: Cannot access 'z' before initialization
+let z = 100;
+console.log(z);
+
+
+/**
+ * const usage
+ */
+
+//Re-declararation is not allowed
+const name = 'Test';
+name = 'Tester'; //TypeError: Assignment to constant variable.
+
+const PI = 3.14;
+PI = 4;
+
+//Duplicate variable is not allowed
+const name = 'QA';
+
+//Hoisting is not allowed
+console.log(size); //ReferenceError: Cannot access 'size' before initialization
+const size = 10;
+console.log(size);
